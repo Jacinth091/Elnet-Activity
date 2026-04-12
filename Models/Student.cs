1ca23b0b8625f108e8069aac5c897cbb45f50c1b
Jacinth <jacinthcedricbarral@gmail.com>
2026-04-12 14:35:22 +0800

    feat[Jacinth]: Refactor student forms to use searchable dropdown

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Barral_ELNET1_MVC.Models
 {
@@ -6,23 +7,23 @@ namespace Barral_ELNET1_MVC.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Fullname is required.")]
-        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be  between 2 and 100 characters long.")]
+        [Required(ErrorMessage = "Full Name is required.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 100 characters.")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Course is required.")]
+        [Required(ErrorMessage = "Course selection is required.")]
         public string Course { get; set; }
 
         [Required(ErrorMessage = "Age is required.")]
-        [Range(18, 100, ErrorMessage = "Age must be  between 18 and 100.")]
+        [Range(18, 60, ErrorMessage = "Age must be between 18 and 60.")]
         public int Age { get; set; }
 
-        [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Birth Date is required.")]
+        [DataType(DataType.Date)]
         public DateOnly BirthDate { get; set; }
-
     }
 }
